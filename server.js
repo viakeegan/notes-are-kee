988614fbaf96849e374e9e0882258824780607d0
unknown <viakeegan@gmail.com>
2022-01-13 02:00:52 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
