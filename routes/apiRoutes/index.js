f6fe310e7d8660d9635af95e8dea91b71676b000
unknown <viakeegan@gmail.com>
2022-01-13 01:40:08 -0500

    finalize apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -11,3 +11,17 @@ router.get("/notes", (req, res) => {
     let results = notes;
     res.json(results);
   });
+
+  router.post("/notes", (req, res) => {
+    req.body.id = uuidv4();
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+  });  
+
+  router.delete("/notes/:id" , (req, res) => {
+    const params = req.params.id
+    updateDb(params, notes);
+    res.redirect('');
+  });
+
+  module.exports = router;
