139da1b1c8c15cb99672c2a89d229e3d2f1cd54c
unknown <viakeegan@gmail.com>
2022-01-13 01:07:43 -0500

    update server.js file

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
+// parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
 app.use(express.json());
 
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}. Welcome!`);
+  });
