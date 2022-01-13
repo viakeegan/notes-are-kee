630c3b32f030ef8fc00c5321286b493e872b138f
unknown <viakeegan@gmail.com>
2022-01-13 01:03:56 -0500

    incoming string array json data

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
